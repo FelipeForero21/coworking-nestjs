a42a17929999f822ec0aea336246270fd2ad4fe1
FelipeForero21 <elipeforero21@gmail.com>
2024-06-21 09:50:34 -0500

    Agregate to swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Coworking API')
+    .setDescription('API para gestionar espacios de trabajo en un coworking')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
